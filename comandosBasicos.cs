b899148336aa002d8ff44131b375eaf332401b6b
Ricardo Jose Fellini <57669094+ricardofellini@users.noreply.github.com>
2020-08-24 13:59:28 -0300

    In development

---
+++ comandosBasicos.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class comandosBasicos : MonoBehaviour
+{
+
+    public void carregaCena(string nomeCena)
+    {
+
+        SceneManager.LoadScene(nomeCena);
+
+
+    }
+}
---
+++ moveOffSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffSet : MonoBehaviour
+{
+
+
+    private Material materialAtual;
+    public float velocidade;
+    public float offSet;
+
+    void Start()
+    {
+
+
+        materialAtual = GetComponent<Renderer>().material;
+
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+
+        offSet += 0.001f;
+        materialAtual.SetTextureOffset ("_MainTex", new Vector2(offSet * velocidade, 0));
+
+
+    }
+}
